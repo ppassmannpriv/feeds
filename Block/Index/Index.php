7225c6a1efa1961c9350503b69bac1439faaf421
Pieter Paßmann (team in:medias Aachen) <pieter.passmann@inmedias.de>
2017-04-28 12:30:04 +0200

    init

---
+++ Block/Index/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Sync your products to different portals like google shopping
+ * Copyright (C) 2017 magebay
+ * 
+ * This file included in Scriptkid/Storefeeds is licensed under OSL 3.0
+ * 
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Scriptkid\Storefeeds\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ Controller/Index/Index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Sync your products to different portals like google shopping
+ * Copyright (C) 2017 magebay
+ * 
+ * This file included in Scriptkid/Storefeeds is licensed under OSL 3.0
+ * 
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Scriptkid\Storefeeds\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Action\Context  $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
